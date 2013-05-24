2207aba9ec846d13ca1d24099efe25613a5b8b38
Thibault <thibaultfrancois1@gmail.com>
2013-05-24 10:38:12 +0200

    amb expression working

--- scheme/td2/td2.rkt
+++ scheme/td2/td2.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(define (fail) ((error "No more alternatives"))
+(define (fail) (error "No more alternatives"))
 
 (define-syntax amb
   (syntax-rules ()
@@ -11,7 +11,24 @@
        (let/cc k
          (set! fail (lambda ()
                       (set! fail old-fail)
-                      (k (lambda () y))))
-         (lambda () x))))
+                      (k (begin y))))
+         x)))
     ((amb x y ...) (amb x (amb y ...)))
     ))
+
+; Function to help compiler understand that fail is not a constant
+(define (foobar)
+  (amb 1 2))
+
+(define (mult)
+  (let ((x (amb 1 2 3 4 5 6 7 8 9 10))
+        (y (amb 1 2 3 4 5 6 7 8 9 10)))
+     (if (= (* x y) 30) (list x y) (amb))))
+(mult)
+
+(define (produce n)
+  (printf "Producing ~s~n" n)
+  n)
+
+; Test to check if we don't evaluate all the arguments
+(amb (produce 1) (produce 2) (produce 3))

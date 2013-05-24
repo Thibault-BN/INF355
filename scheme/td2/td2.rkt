3fa04704398ca00b4c25f64a0600be1ff7e8ab15
Thibault <thibaultfrancois1@gmail.com>
2013-05-24 09:56:08 +0200

    amb expression (not working yet)

--- scheme/td2/td2.rkt
+++ scheme/td2/td2.rkt
@@ -1,3 +1,16 @@
 #lang racket
 
-(define (fail) (error "no more alternatives"))
+(define (fail) ((error "No more alternatives"))
+
+(define-syntax amb
+  (syntax-rules ()
+    ((amb) (fail))
+    ((amb x) x)
+    ((amb x y)
+     (let ((old-fail fail))
+       (let/cc k
+         (set! fail (lambda ()
+                      (set! fail old-fail)
+                      (k (lambda () y))))
+         (lambda () x))))
+    ))

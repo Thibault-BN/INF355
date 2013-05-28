42a427a58b0417db351f3161642d8b4403f97b0a
Thibault <thibaultfrancois1@gmail.com>
2013-05-28 16:04:22 +0200

    Working RPN Calculator

--- haskell/td1/td1.hs
+++ haskell/td1/td1.hs
@@ -1,4 +1,6 @@
-module Td1 where
+module Main where
+
+import System.IO
 
 type Stack = [Int]
 type Operator = Stack -> Stack
@@ -13,8 +15,22 @@ parseOp "swap" = \s -> [(s !! 1), head s] ++ (drop 2 s)
 parseOp "drop" = \s -> tail s
 parseOp "depth" = \s -> [length s] ++ s
 parseOp "pick" = \s -> [s !! (head s + 1)] ++ tail s
+parseOp "clear" = \s -> []
 parseOp x = \s -> [(read x) :: Int] ++ s
 
 eval :: Stack -> [Operator] -> Stack
 eval s [] = s
 eval s (x : xs) = eval (x s) xs
+
+parse :: String -> [Operator]
+parse line = map (parseOp) (words line)
+
+repl :: Stack -> IO ()
+repl stack = do
+  putStr "> "
+  hFlush stdout
+  line <- getLine
+  newstack <- return $ eval stack (parse line)
+  putStrLn $ show $ reverse newstack
+  repl newstack
+main = repl []

6eb428625f3902806479d1f7b43f96694359ae25
Thibault <thibaultfrancois1@gmail.com>
2013-05-28 15:30:42 +0200

    Deleting tests in parseOp

--- haskell/td1/td1.hs
+++ haskell/td1/td1.hs
@@ -4,17 +4,13 @@ type Stack = [Int]
 type Operator = Stack -> Stack
 
 parseOp :: String -> Operator
-parseOp "+" = \s -> if (length s > 1)
-                       then [(s !! 0) + (s !! 1)] ++ (drop 2 s) else s
-parseOp "*" = \s -> if (length s > 1)
-                       then [(s !! 0) * (s !! 1)] ++ (drop 2 s) else s
-parseOp "-" = \s -> if (length s > 1)
-                       then [(s !! 0) - (s !! 1)] ++ (drop 2 s) else s
-parseOp "/" = \s -> if (length s > 1)
-                       then [(s !! 0) `div` (s !! 1)] ++ (drop 2 s) else s
-parseOp "dup" = \s -> if (length s > 0)
-                         then [s !! 0] ++ s else s
-parseOp "swap" = \s -> if (length s > 1)
-                          then [(s !! 1), (s !! 0)] ++ (drop 2 s) else s
-parseOp "drop" = \s -> if (length s > 1)
-                          then drop 1 s else []
+parseOp "+" = \s -> [head s + (s !! 1)] ++ (drop 2 s)
+parseOp "*" = \s -> [head s * (s !! 1)] ++ (drop 2 s)
+parseOp "-" = \s -> [head s - (s !! 1)] ++ (drop 2 s)
+parseOp "/" = \s -> [head s `div` (s !! 1)] ++ (drop 2 s)
+parseOp "dup" = \s -> [head s] ++ s
+parseOp "swap" = \s -> [(s !! 1), head s] ++ (drop 2 s)
+parseOp "drop" = \s -> tail s
+parseOp "depth" = \s -> [length s] ++ s
+parseOp "pick" = \s -> [s !! (head s + 1)] ++ tail s
+parseOp x = \s -> [(read x) :: Int] ++ s

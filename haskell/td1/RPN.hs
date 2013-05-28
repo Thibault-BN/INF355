29f0c39294cb7fd5b954532a4e05cec9a7dc50bf
Thibault <thibaultfrancois1@gmail.com>
2013-05-28 19:03:57 +0200

    Passing all tests for RPN

--- haskell/td1/RPN.hs
+++ haskell/td1/RPN.hs
@@ -2,21 +2,21 @@ module RPN where
 
 import System.IO
 
-type Stack = [Int]
+type Stack = [Integer]
 type Operator = Stack -> Stack
 
 parseOp :: String -> Operator
 parseOp "+" = \s -> [head s + (s !! 1)] ++ (drop 2 s)
 parseOp "*" = \s -> [head s * (s !! 1)] ++ (drop 2 s)
-parseOp "-" = \s -> [head s - (s !! 1)] ++ (drop 2 s)
-parseOp "/" = \s -> [head s `div` (s !! 1)] ++ (drop 2 s)
+parseOp "-" = \s -> [(s !! 1) - head s] ++ (drop 2 s)
+parseOp "/" = \s -> [(s !! 1) `div` head s] ++ (drop 2 s)
 parseOp "dup" = \s -> [head s] ++ s
 parseOp "swap" = \s -> [(s !! 1), head s] ++ (drop 2 s)
 parseOp "drop" = \s -> tail s
-parseOp "depth" = \s -> [length s] ++ s
-parseOp "pick" = \s -> [s !! (head s + 1)] ++ tail s
+parseOp "depth" = \s -> [toInteger(length s)] ++ s
+parseOp "pick" = \s -> [s !! fromInteger(head s + 1)] ++ tail s
 parseOp "clear" = \s -> []
-parseOp x = \s -> [(read x) :: Int] ++ s
+parseOp x = \s -> [(read x) :: Integer] ++ s
 
 eval :: a -> [a -> a] -> a
 eval s [] = s

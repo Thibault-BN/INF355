ba6ba3536fd2821ab8eba5a1cef64517559d4f83
Thibault <thibaultfrancois1@gmail.com>
2013-05-28 20:19:33 +0200

    Adding Peano definition only miss show-read test

--- haskell/td1/CheckPeano.hs
+++ haskell/td1/CheckPeano.hs
@@ -1,4 +1,4 @@
-module CheckPeano where
+module Main where
 
 import Control.Monad
 import Peano
--- haskell/td1/Peano.hs
+++ haskell/td1/Peano.hs
@@ -1,6 +1,6 @@
 module Peano where
 
-data Peano = Zero | Succ Peano deriving (Eq)
+data Peano = Zero | Succ Peano deriving (Eq, Read, Ord)
 
 instance Num Peano where
   a + Zero = a
@@ -10,6 +10,8 @@ instance Num Peano where
   a * Succ b = a + (a * b)
   
   a - Zero = a
+  --case needed for mul div test
+  Zero - b = b
   Succ a - Succ b = (a - b)
   
   abs a = a
@@ -24,3 +26,21 @@ instance Show Peano where
   show Zero = "Zero"
   show (Succ Zero) = "Succ Zero"
   show (Succ a) = "Succ (" ++ (show a) ++ ")"
+  
+instance Enum Peano where
+  toEnum 0 = Zero
+  toEnum i = Succ (toEnum (i-1))
+  
+  fromEnum Zero = 0
+  fromEnum (Succ a) = 1 + (fromEnum a)
+  
+instance Integral Peano where
+  toInteger Zero = 0
+  toInteger (Succ a) = 1 + (toInteger a)
+  
+  quotRem Zero _ = (Zero, Zero)
+  quotRem a b = (fromInteger(toInteger(a) `div` toInteger(b)), fromInteger(toInteger(a) `rem` toInteger(b)))
+  
+instance Real Peano where
+  toRational Zero = 0
+  toRational (Succ a) = 1 + (toRational a)

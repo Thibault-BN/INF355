d1295f7de334e8e2fc25dd214a5bb33921e42165
Thibault <thibaultfrancois1@gmail.com>
2013-06-04 14:26:45 +0200

    Cleaned version

--- haskell/td2/td2.hs
+++ haskell/td2/td2.hs
@@ -4,8 +4,6 @@ import Data.Ratio
 
 data Prob a = Prob [(a, Rational)] deriving (Show)
 
-createCouple l x = (x, 1%l)
-
 sameProbability :: [a] -> Prob a
-sameProbability [] = Prob []
 sameProbability l = Prob (map (\x -> (x, 1%(toInteger $ length l))) l)
+

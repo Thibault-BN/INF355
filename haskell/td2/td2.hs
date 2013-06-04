951403d59ca2e128de2dd119f0acaf40bdf7e208
Thibault <thibaultfrancois1@gmail.com>
2013-06-04 15:24:43 +0200

    Beginning of Monad definition

--- haskell/td2/td2.hs
+++ haskell/td2/td2.hs
@@ -2,8 +2,19 @@ module Td2 where
 
 import Data.Ratio
 
-data Prob a = Prob [(a, Rational)] deriving (Show)
+data Prob a = Prob { getList :: [(a, Rational)]} deriving (Show)
 
 sameProbability :: [a] -> Prob a
-sameProbability l = Prob (map (\x -> (x, 1%(toInteger $ length l))) l)
+sameProbability l = Prob [(x, 1%(toInteger $ length l)) | x <- l]
 
+conc :: Prob a -> Prob a -> Prob a
+conc (Prob l1) (Prob l2) = Prob (l1 ++ l2)
+
+instance Functor Prob where
+  fmap f (Prob [(a,r)]) = Prob [(f a, r)]
+  fmap f (Prob (x : xs)) = (fmap f (Prob [x])) `conc` (fmap f (Prob xs))
+  
+instance Monad Prob where
+  return x = Prob [(x, 1)]
+  Prob [] >>= _ = Prob []
+  Prob [(a,r)] >>= f = Prob [(x, y * r) | (x, y) <- (getList (f a))]
